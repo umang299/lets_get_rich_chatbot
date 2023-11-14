1e7b65a564665bd70f6a36e014e9e03290960425
umang444 <umang@adeodist.com>
2023-11-14 18:04:32 +0530

    mod: utility functions to manage conversation history.

--- utils.py
+++ utils.py
@@ -1,17 +1,24 @@
 import os
 import re
 import json
+from uuid import uuid4
 from datetime import datetime
 
 
 
-def save_json(dictionary):
-    dst_path = os.path.join("conversation", f"{dictionary['id']}.json")
+def logger(message, role):
+    payload = {
+            'Id' : str(uuid4()),
+            'Role' : role,
+            'Timestamp' : datetime.isoformat(datetime.now()),
+            'Message' : message}
+    
+    dst_path = os.path.join("conversation", f"{payload['Id']}.json")
     with open(dst_path, 'w') as f:
-        json.dump(dictionary, f)
+        json.dump(payload, f)
 
 
-def load_conversation():
+def load_conversation(top_n):
     data_list = []
     for filename in os.listdir("conversation"):
         if filename.endswith(".json"):
@@ -21,11 +28,12 @@ def load_conversation():
                 data_list.append(data)
 
     
-    data_list.sort(key=lambda x: datetime.strptime(x["timestamp"], "%a %b %d %H:%M:%S %Y"), reverse=False)
+    data_list.sort(key=lambda x: datetime.strptime(x["Timestamp"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=False)
+
 
     output_string = ""
-    for data in data_list:
-        output_string += f"{data['entity']} : {data['message'].strip()} \n"
+    for data in data_list[:top_n*2]:
+        output_string += f"{data['Role']} : {data['Message'].strip()} \n"
 
     return output_string
 
