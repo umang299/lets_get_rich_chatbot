09fa2d86c8fde29a859bb6ddc176480429028b38
umang444 <umang@adeodist.com>
2023-11-14 18:05:37 +0530

    refactor: refactored code.

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import dotenv
-from uuid import uuid4
-from datetime import datetime
 from assistant import Assistant
 
-import streamlit as st
-from streamlit_chat import message
+import warnings
 
-from utils import save_json
 
+warnings.filterwarnings("ignore")
 env_var = dotenv.dotenv_values(".env")
 assistant = Assistant(openai_api=env_var['OPENAI_API_KEY'], 
                       pinecone_api=env_var['PINECONE_API_KEY'])
     
 if __name__ == "__main__":
-    msg = str(input("USER: "))
-    response = assistant.response(query=msg)
-    print(response)
+    while True:
+        msg = str(input("USER: "))
+        response, sources = assistant.response(query=msg)
+        print(f"ASSISTANT: {response}\nSources: {sources}")
